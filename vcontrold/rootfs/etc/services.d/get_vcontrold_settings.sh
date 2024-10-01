131e0ce6f97c0091a6a2840ab5f7e5fe2d68a037
Alexandre <Alexandre-io@users.noreply.github.com>
2024-10-01 16:13:31 +0200

    refactor(plugin): improve code structure and performance

--- vcontrold/rootfs/etc/services.d/get_vcontrold_settings.sh
+++ vcontrold/rootfs/etc/services.d/get_vcontrold_settings.sh
@@ -4,10 +4,20 @@
 export REFRESH_RATE=$(bashio::config 'refresh')
 
 # Fetch VCONTROL_HOST and set to localhost by default if not provided
-export VCONTROL_HOST=$(bashio::config 'vcontrol_host' || echo "localhost")
+if bashio::config.has_value 'vcontrol_host'; then
+    export VCONTROL_HOST=$(bashio::config 'vcontrol_host')
+else
+    bashio::log.warning "vcontrol_host not set in configuration. Defaulting to 'localhost'."
+    export VCONTROL_HOST="localhost"
+fi
 
 # Fetch VCONTROL_PORT and set to 3002 by default if not provided
-export VCONTROL_PORT=$(bashio::config 'vcontrol_port' || echo "3002")
+if bashio::config.has_value 'vcontrol_port'; then
+    export VCONTROL_PORT=$(bashio::config 'vcontrol_port')
+else
+    bashio::log.warning "vcontrol_port not set in configuration. Defaulting to '3002'."
+    export VCONTROL_PORT="3002"
+fi
 
 # Log the configured VCONTROL host and port
 bashio::log.info "vcontrold will be set to host: $VCONTROL_HOST and port: $VCONTROL_PORT"
